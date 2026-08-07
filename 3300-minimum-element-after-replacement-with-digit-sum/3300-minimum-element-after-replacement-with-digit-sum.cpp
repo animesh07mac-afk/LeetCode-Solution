20d078f59e662c8a260066037126aa80346ded8a
animesh07mac-afk <animesh07mac@gmail.com>
2026-08-07 15:05:21 +0530

    Time: 0 ms (100%), Space: 27.8 MB (52.99%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minElement(vector<int>& nums) {
+        int mini=INT_MAX;
+        
+        for(int i=0;i<nums.size();i++){
+            int n=nums[i];
+            int sum=0;
+            while(n>0){
+                sum=sum+n%10;
+                n=n/10;
+
+            }
+            mini=min(sum,mini);
+            
+        }
+        return mini;
+        
+    }
+};
