4846060c0d094aa8c86ab125c12ef404e05f9fb5
animesh07mac-afk <animesh07mac@gmail.com>
2026-08-19 01:09:26 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.94%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum=0;
+        int y=x;
+        while(y>0){
+            sum+=y%10;
+            y=y/10;
+        }
+        if(x%sum==0) return sum;
+        return -1;
+        
+    }
+};
