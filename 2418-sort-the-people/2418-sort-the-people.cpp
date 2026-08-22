f0c5d3372ec5f199bbca0467bbb5f9b09e1c6cf3
animesh07mac-afk <animesh07mac@gmail.com>
2026-08-22 14:27:26 +0530

    Time: 7 ms (81.74%), Space: 29.2 MB (30.64%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int,string>mpp;
+        for(int i=0;i<heights.size();i++){
+            mpp[heights[i]]=names[i];
+        }
+        vector<string>ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
